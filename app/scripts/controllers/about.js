8a095afa03213b9cc38ec8b3589c6e09bcd15803
scheibapps@gmail.com <scheibapps@gmail.com>
2016-06-16 20:52:54 -0400

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name scheibappsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the scheibappsApp
+ */
+angular.module('scheibappsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name scheibappsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the scheibappsApp
+ */
+angular.module('scheibappsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
